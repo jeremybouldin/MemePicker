02fdd45de9c11a74cef41fdadba4d3604fde4f72
jeremybouldin <56416889+jeremybouldin@users.noreply.github.com>
2022-12-06 17:25:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ import { catsData } from "/data.js"
 const emotionRadios = document.getElementById('emotion-radios')
 const getImgBtn = document.getElementById('get-image-btn')
 const gifsOnlyCheckbox = document.getElementById('gifs-only-option')
+const memeModal = document.getElementById('meme-modal')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModalClose = document.getElementById('meme-modal-close-btn')
 
+memeModalClose.addEventListener('click', hideModal)
 
-getImgBtn.addEventListener('click', renderCat)
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
+getImgBtn.addEventListener('click', renderCat)
+
+function hideModal(){
+    memeModal.style.display = 'none'
+}
 
 function highlightCheckedOption(e){
     // first remove the highlighting from all other radios
@@ -17,6 +25,26 @@ function highlightCheckedOption(e){
     
     // next assign highlighting to the selected radio
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+//renders out the cat image/gif
+function renderCat(){
+    const catObject = getSingleCatObject()
+    memeModal.style.display = 'flex'
+    memeModalInner.innerHTML = `
+        <img class="cat-img" src="./images/${catObject.image}" alt="${catObject.alt}">
+    `
+}
+
+//Narrows down the array to a single object selected randomly
+function getSingleCatObject(){
+    const catsArray = getMatchingCatsArray()
+    if (catsArray.length === 1){
+        console.log(catsArray[0])
+    } else {
+        const randomNumber = Math.floor(Math.random()*catsArray.length)
+        return catsArray[randomNumber]
+    }
 }
 
 //returns an array matching user's criteria
@@ -33,22 +61,6 @@ function getMatchingCatsArray() {
         })
         return matchingCatsArray
     }
-}
-
-//Narrows down the array to a single object selected randomly
-function getSingleCatObject(){
-    const catsArray = getMatchingCatsArray()
-    if (catsArray.length === 1){
-        console.log(catsArray[0])
-    } else {
-        const randomNumber = Math.floor(Math.random()*catsArray.length)
-        return catsArray[randomNumber]
-    }
-}
-
-//renders out the cat image/gif
-function renderCat(){
-    getSingleCatObject()
 }
 
 function getEmotionsArray(cats) {
